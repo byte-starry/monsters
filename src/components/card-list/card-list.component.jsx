b4657f51fed55cbd03c58a5695fdbfcc9d505675
Shamita Deshmukh <shamita.deshmukh@gmail.com>
2022-04-20 16:38:26 +0530

    commit_3

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,22 @@
+import Card from '../card/card.component'
+import './card-list.styles.css'
+
+const CardList = (props) => {
+    const {monsters} = props
+    return(
+        <div className='card-list'>
+            {
+                monsters.map(
+                    monster => <Card 
+                                    id = {monster.id}
+                                    name = {monster.name} 
+                                    email = {monster.email}
+                                />
+                )
+            }
+        </div>
+        
+    )
+}
+
+export default CardList
---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,17 @@
+import './search.styles.css'
+
+const Search = () => {
+
+    return (
+        <>
+            <input
+            className="search-box" 
+            type="text" 
+            placeholder="Search Monsters"
+            />
+        </>
+    )
+}
+
+export default Search
+
